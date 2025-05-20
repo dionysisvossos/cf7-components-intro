d00fcf63488ccdb98700283df8f38ad9b7c9ea69
Dionysis.Vossos <dionysis.vossos@windowslive.com>
2025-05-20 21:01:34 +0300

    feat: new components created

---
+++ src/components/ArrowFunctionalComponent.tsx
@@ -0,0 +1,11 @@
+const ArrowFunctionalComponent = () => {
+    const title: string = "Is an arrow functional component";
+    return <h1 className="text-center mt-12 text-xl font-bold">{title}</h1>;}
+// This is a functional component defined using an arrow function.
+// It is a more concise way to write functional components in React.
+// Arrow functions are often used for functional components in modern React development.
+// They provide a shorter syntax and can help avoid issues with the `this` keyword in class components.
+// Arrow functions are also more readable and easier to understand for many developers.
+// Arrow functions are a feature of ES6 (ECMAScript 2015) and are widely used in modern JavaScript development.
+
+export default ArrowFunctionalComponent;
---
+++ src/components/FunctionalComponent.tsx
@@ -0,0 +1,7 @@
+function FunctionalComponent() {
+    const title = "Is a functional component";
+    return <h1 className="text-center mt-12 text-xl font-bold">{title}</h1>;
+}
+// This is a functional component in React
+
+export default FunctionalComponent;
