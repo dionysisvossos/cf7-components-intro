0395f9b1ffc2b53614686c85e60b6f2e83a01952
Dionysis.Vossos <dionysis.vossos@windowslive.com>
2025-05-21 20:26:48 +0300

    feat: using states in components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
 // import ViteIntro from "./pages/ViteIntro.tsx";
 
-import ClassComponent from "./components/ClassComponent.tsx";
-import FunctionalComponent from "./components/FunctionalComponent.tsx";
-import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
-import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
-import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
+// import ClassComponent from "./components/ClassComponent.tsx";
+// import FunctionalComponent from "./components/FunctionalComponent.tsx";
+// import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+// import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
+// import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
+
+
+import Layout from "./components/Layout.tsx";
+// import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
+import Counter from "./components/Counter.tsx";
+// import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
 
 function App() {
 
   return (
     <>
-        <ClassComponent />
-        <FunctionalComponent />
-        <ArrowFunctionalComponent />
-        <ArrowFunctionalComponentWithProps
-            title="Is an arrow functional component with props" />
-        <ArrowFunctionalComponentWithPropsType
-            title="Is an arrow functional component with props"
-            description="This is a description"
-        />
-      {/*<ViteIntro />*/}
+        <Layout>
+      {/*  <ClassComponent />*/}
+      {/*  <FunctionalComponent />*/}
+      {/*  <ArrowFunctionalComponent />*/}
+      {/*  <ArrowFunctionalComponentWithProps*/}
+      {/*      title="Is an arrow functional component with props" />*/}
+      {/*  <ArrowFunctionalComponentWithPropsType*/}
+      {/*      title="Is an arrow functional component with props"*/}
+      {/*      description="This is a description"*/}
+      {/*  />*/}
+      {/*/!*<ViteIntro />*!/*/}
+
+            {/*<ClassComponentWithState/>*/}
+            {/*<FunctionalComponentWithState/>*/}
+            <Counter/>
+        </Layout>
     </>
   )
 }
