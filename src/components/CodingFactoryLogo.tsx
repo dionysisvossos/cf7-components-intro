9ca48a9c7995d3a8a1a2e6f968b2af857ab1b394
Dionysis.Vossos <dionysis.vossos@windowslive.com>
2025-05-21 20:21:28 +0300

    feat: add cf logo

---
+++ src/components/CodingFactoryLogo.tsx
@@ -0,0 +1,9 @@
+const CodingFactoryLogo = () => {
+    return (
+        <>
+        <img src="/logo.png" alt="CF7" className="my-4 h-16"/>
+        </>
+    )
+}
+
+export default CodingFactoryLogo;
