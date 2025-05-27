a711dfa42174ad1a420da6fb3b1c665b432cebf3
Dionysis.Vossos <dionysis.vossos@windowslive.com>
2025-05-27 21:23:38 +0300

    feat: create to-do app

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@
 // import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
 // import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
 import Layout from "./components/Layout.tsx";
-import CounterWithReducer from "./components/CounterWithReducer.tsx";
+// import CounterWithReducer from "./components/CounterWithReducer.tsx";
 // import CounterAdvanced from "./components/CounterAdvanced.tsx";
 // import CounterWithCustomHook from "./components/CounterWithCustomHook.tsx";
 // import CounterAdvancedWithCustomHook from "./components/CounterAdvancedWithCustomHook.tsx";
@@ -15,6 +15,7 @@ import CounterWithReducer from "./components/CounterWithReducer.tsx";
 // import NameChanger from "./components/NameChanger.tsx";
 // import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
 // import CounterWithMoreStates from "./components/CounterWithMoreStates.tsx";
+import Todo from "./components/Todo/Todo.tsx";
 
 
 function App() {
@@ -40,7 +41,8 @@ function App() {
         {/*<CounterAdvanced/>*/}
         {/*<CounterWithCustomHook/>*/}
         {/*<CounterAdvancedWithCustomHook/>*/}
-        <CounterWithReducer/>
+        {/*<CounterWithReducer/>*/}
+        <Todo/>
         </Layout>
     </>
   )
