1ddd620804b54f0bc62e34550dc77c2d0d10330a
Dionysis.Vossos <dionysis.vossos@windowslive.com>
2025-06-10 18:17:13 +0300

    feat: new examples page/section/routes

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,9 @@ import NameChangerPage from "./pages/NameChangerPage.tsx";
 import OnlineStatusPage from "./pages/OnlineStatusPage.tsx";
 import UserPage from "./pages/UserPage";
 import RouterLayout from "./components/RouterLayout.tsx";
+import ExamplesPage from "./pages/ExamplesPage.tsx";
+import RouterExamplesLayout from "./components/RouterExamplesLayout.tsx";
+
 
 
 
@@ -62,26 +65,30 @@ function App() {
         {/*</Layout>*/}
 
         <BrowserRouter>
-            {/*<Layout>*/}
             <Routes>
-                <Route element={<RouterLayout />} >
+                <Route element={<RouterLayout />}>
+                    {/*<Route path="/" element={<HomePage />}/>*/}
                     <Route index element={<HomePage />}/>
+                    <Route path="users/:userId" element={<UserPage />}/>
+                    <Route path="users" element={<UserPage />}/>
                 </Route>
 
-
-                {/*<Route path="example/name-changer" element={<NameChangerPage />}/>*/}
-                {/*<Route path="example/online-status" element={<NameChangerPage />}/>*/}
-
-                <Route path="examples?">
-                    <Route path="name-changer" element={<NameChangerPage />} />
-                    <Route path="online-status" element={<OnlineStatusPage />} />
+                {/*<Route path="examples?" >*/}
+                <Route path="examples"  element={<RouterExamplesLayout/>}>
+                    <Route index element={<ExamplesPage/>}/>
+                    <Route path="name-changer" element={<NameChangerPage/>}/>
+                    <Route path="online-status" element={<OnlineStatusPage/>}/>
                 </Route>
+
                 <Route path="users/:userId" element={<UserPage />}/>
                 <Route path="users" element={<UserPage />}/>
-                {/*<Route path="files/*" element={<FilePage />}/>*/}
+                {/*<Route path="files/*" element={<FilePage/>}/>*/}
+                PATH: https://example.com/users/125/name/nick
+                QUERY: https://example.com/users?id=125&name=Nick
+                https://www.skroutz.gr/c/3074/pagomixanes/f/891854_891908_1066530/trima-epagelmatiki-101-200.html?price_max=3200.0&price_min=1400.001
             </Routes>
-            {/*</Layout>*/}
         </BrowserRouter>
+
     </>
   )
 }
